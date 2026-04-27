7b9cad984369cee9edd2fd51ebd4d1f341002050
Liuz043 <a2578157128@gmail.com>
2026-04-27 20:26:44 +0800

    improved log and  some codes

--- copy_scr/main_noncoop.m
+++ copy_scr/main_noncoop.m
@@ -2,7 +2,7 @@ clear;
 clc;
 close all;
 
-rng(2026); % 设置随机数种子，确保结果可复现
+rng(202); % 设置随机数种子，确保结果可复现
 
 run('draw_scene.m');
 run('Wind_Turbine.m');
@@ -13,3 +13,8 @@ disp('非合作阶段成果');
 disp(['风电非合作最大利润 = ', num2str(WT_noncoop_ideal_profit)]);
 disp(['光伏非合作最大利润 = ', num2str(PV_noncoop_ideal_profit)]);
 disp(['电制氢非合作最小成本 = ', num2str(H_noncoop_ideal_cost)]);
+
+disp(sol_P_ba_dis);
+disp(max(sol_P_ba_dis));
+disp(sum(sol_P_ba_dis));
+disp(sol_E_ba);
